78934f65e8e9128d7c1210a1a648084d27adb08a
Artem Saprykin <asaprykin17@gmail.com>
2019-07-17 11:22:29 +0300

    Create skeleton project

---
+++ hello_world/server.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+
+from flask import (
+    Flask,
+    jsonify,
+)
+
+server = Flask(__name__)
+
+
+@server.route('/hello-world', methods=['GET'])
+def hello_world():
+    return jsonify({'result': 'hello_world'}), HTTPStatus.OK
+
+
+if __name__ == '__main__':
+    server.run(debug=True, host='0.0.0.0', port=8000)
+
